6e28fa12598082ad10f3679c275407d90d234135
[SW5] 이윤지 <dbswl342@gmail.com>
2024-04-11 17:05:16 +0900

    메인, 메뉴 파일 생성

--- MyDbApp/src/Main.java
+++ MyDbApp/src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		Menu menu = new Menu();
+		menu.run(sc);
 	}
-
 }
---
+++ MyDbApp/src/Menu.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Menu {
+	public void run(Scanner sc) {
+		
+	}
+}
